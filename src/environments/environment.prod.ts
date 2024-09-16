4ff26c16baf0268d299a5ebe0fde844b267ce985
Karam Abu Judom <karam.abujudom@gmail.com>
2024-09-16 13:20:58 +0200

    Change production URL to make backend requests to HTTPS after obtaining an SSL certificate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://157.230.122.239:3000/api/blogs'  // Production URL
+    apiUrl: 'https://kabuwebsite.ddns.net/api/blogs'  // Production URL
   };
   
