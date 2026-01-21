a78b5f676f647045293672de2077235f2ec61a32
Karam Abu Judom <karam.abujudom@gmail.com>
2026-01-21 23:54:54 +0100

    Remove blog component and logic, redirect blog requests to external bearblog website, move navigation to top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -7,16 +8,28 @@ import { MatButtonModule } from '@angular/material/button'; // For buttons
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatLine} from "@angular/material/core";
-import {MatIconButton} from "@angular/material/button";
+import { MatLine } from '@angular/material/core';
+import { MatIconButton } from '@angular/material/button';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, MatSidenavModule, MatIconModule, MatButtonModule, MatToolbar, MatListModule, MatToolbarModule, MatLine, MatIconButton],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbar,
+    MatListModule,
+    MatToolbarModule,
+    MatLine,
+    MatIconButton,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   isMobile = false;
@@ -26,11 +39,13 @@ export class AppComponent implements OnInit {
   constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {
-    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
-      this.isMobile = result.matches;
-    });
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+      });
   }
- 
+
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
     document.body.classList.toggle('dark-theme', this.isDarkMode);
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
-import { BlogComponent } from './pages/blog/blog.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ResumeComponent } from './pages/resume/resume.component';
-import { BlogPostComponent } from './blog-post/blog-post.component';
-import { AdminCreatePostComponent } from './admin/admin-create-post/admin-create-post.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default to home
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'blog', component: BlogComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'blog/:slug', component: BlogPostComponent }, // Single blog post
   { path: 'resume', component: ResumeComponent },
-  { path: 'admin/create-post', component:AdminCreatePostComponent },
-  { path: 'admin/edit-post/:slug', component:AdminCreatePostComponent },
-  { path: '**', redirectTo: '' } // Fallback to home on invalid routes
+  { path: '**', redirectTo: '' }, // Fallback to home on invalid routes
 ];
